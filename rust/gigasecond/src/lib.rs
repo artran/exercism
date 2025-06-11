8cd38db07476aeb25df5e619815717544b226fb6
Ray Tran <rt@elixirsoftware.co.uk>
2025-06-11 23:22:07 +0200

    Clock challenge

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -3,6 +3,6 @@ use time::{Duration, PrimitiveDateTime as DateTime};
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
     let one_gig: i64 = 1_000_000_000;
-    let one_gigasec = Duration::new(one_gig, 0);
+    let one_gigasec = Duration::seconds(one_gig);
     start + one_gigasec
 }
