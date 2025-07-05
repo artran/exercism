c814adefd1fcf9d7c514ce42d57fd483da37e3a4
Ray Tran <ray@artran.co.uk>
2025-07-05 17:06:55 +0100

    Implement constructor

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,8 +1,24 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+    pub fn new(mut hours: i32, mut minutes: i32) -> Self {
+        hours += minutes.div_euclid(60);
+        hours = hours.rem_euclid(24);
+        minutes = minutes.rem_euclid(60);
+
+        Clock { hours, minutes }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
