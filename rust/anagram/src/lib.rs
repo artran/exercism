fa945ed8a259bcd62fe40cd2910ef8a6e8818f33
Ray Tran <ray@artran.co.uk>
2025-07-06 11:27:58 +0100

    Get first test to pass

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,5 +1,14 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let matches = possible_anagrams
+        .iter()
+        .cloned()
+        .filter(|c| is_anagram(word, c));
+
+    HashSet::from_iter(matches)
+}
+
+fn is_anagram(_word: &str, _candidate: &str) -> bool {
+    false
 }
