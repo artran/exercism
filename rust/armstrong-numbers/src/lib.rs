008711e87a91a1d954c4bb01e28e064cefaf948f
Ray Tran <ray@artran.co.uk>
2025-07-25 12:53:11 +0100

    Solve armstrong-number challenge

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    let power = match num {
+        0..9 => 1, // Zero would panic in ilog10 and small optimisation including 1..9
+        _ => num.ilog10() + 1,
+    };
+
+    num.to_string()
+        .chars()
+        .map(|x| x.to_digit(10).unwrap().pow(power))
+        .sum::<u32>()
+        == num
 }
